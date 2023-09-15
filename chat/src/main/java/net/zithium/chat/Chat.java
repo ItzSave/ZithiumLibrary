6719d55233aea9f42a6eeee95de808b97bb3bc2f
Save <itzsaveyt@gmail.com>
2023-09-15 18:11:16 -0500

    Attempt three at publishing error.

--- chat/src/main/java/net/zithium/chat/Chat.java
+++ chat/src/main/java/net/zithium/chat/Chat.java
@@ -1,17 +1,16 @@
 package net.zithium.chat;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Chat extends JavaPlugin {
 
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
 
+    public static String color(String message) {
+        Component component = MiniMessage.miniMessage().deserialize(message);
+        return LegacyComponentSerializer.legacySection().serialize(component);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
 }
